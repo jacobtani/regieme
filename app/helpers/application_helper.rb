057d89d4f9879225a235f8dd4a55cbb1c1e96545
Tania Jacob <tanzjacob@gmail.com>
2016-06-09 16:55:05 +1200

    Add flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+ def flash_class(level)
+    case level.to_sym
+    when :notice then "warning-alert"
+    when :success then "alert alert-success"
+    when :error then "danger-alert"
+    when :alert then "danger-alert"
+    end
+  end
 end
