ac58e23546ce8015f8e74f939b1617cf2e04f5d2
Tania Jacob <tanzjacob@gmail.com>
2016-06-17 09:32:21 +1200

    Add rsvp confirmation email to be sent to Iain and Tania too

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -7,4 +7,10 @@ class AdminMailer < ApplicationMailer
     mail(from: @guest.email, to: 'tjterminator.dev@gmail.com', subject: @message.message_subject)
   end
 
+  def rsvp_confirmation(rsvp, guest)
+    @rsvp = rsvp
+    @guest = guest
+    mail(from: @guest.email, to: 'tjterminator.dev@gmail.com', subject: 'RSVP from #{@guest.full_name}')
+  end
+
 end
