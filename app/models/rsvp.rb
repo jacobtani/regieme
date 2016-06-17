c06c78d800914994808096dc81092647bc9d6af0
Tania Jacob <tanzjacob@gmail.com>
2016-06-17 14:06:12 +1200

    Ensure guest only rsvps once

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -4,4 +4,5 @@ class Rsvp < ActiveRecord::Base
   validates_presence_of :guest_id, :message => "Your name was not recognised - please enter your full name"
   validates_presence_of :meal_preference, :if => 'attending == "Yes"'
   attr_accessor :full_name
+  validates_uniqueness_of :guest_id, message: "You have already RSVPed to the wedding. If you need to alter this please message us on our Contact Us page"
 end
