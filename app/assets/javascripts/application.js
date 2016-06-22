fe41cd4e1ec9bdfea4636411b0a0356d7751c5cc
Tania Jacob <tanzjacob@gmail.com>
2016-06-23 10:15:34 +1200

    Remove cocoon from application.js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require flash_messages
-//= require cocoon
 //= require owl.carousel
 //= require bootstrap-sprockets
 //= require turbolinks
