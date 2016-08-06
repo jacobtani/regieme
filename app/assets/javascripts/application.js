8329580a2b13b87152718561859e2a3bb49b5344
Tania Jacob <tanzjacob@gmail.com>
2016-08-06 21:22:42 +1200

    Decrease main people carousel time

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,7 @@
 $(document).ready(function() { 
 
   $('#owl-demo').owlCarousel({
-       autoPlay: 6000, //Set AutoPlay to 3 seconds
+       autoPlay: 4000, //Set AutoPlay to 3 seconds
        items : 4,
       itemsDesktop : [1199,3],
       itemsDesktopSmall : [979,3]
