967c90d96da4acfbcfe956f5abb97d1777377643
Tania Jacob <tanzjacob@gmail.com>
2016-08-18 12:25:13 +1200

    Change static page data to render data from json

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,6 +16,14 @@ class PagesController < ApplicationController
   end
 
   def faq
+    @faqs = JSON.parse((File.read("#{Rails.root}/app/assets/javascripts/faq.json")))
+  end
+
+  def people
+    @main_people = JSON.parse((File.read("#{Rails.root}/app/assets/javascripts/main_party.json")))
+    @bridesmaids = JSON.parse((File.read("#{Rails.root}/app/assets/javascripts/bridesmaids.json")))
+    @groomsmen = JSON.parse((File.read("#{Rails.root}/app/assets/javascripts/groomsmen.json")))
+    @wedding_party = JSON.parse((File.read("#{Rails.root}/app/assets/javascripts/wedding_party.json")))
   end
 
 end
