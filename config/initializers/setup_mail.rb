abdcb3024159d6629fbc0363ff7d8e0a2dfbfdab
Tania Jacob <tanzjacob@gmail.com>
2016-09-12 15:44:41 +1200

    Change email address password

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :user_name            => "tjterminator.dev@gmail.com",
-  :password             => "Jeco551#",
+  :password             => "cat-log-jump-fish",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
