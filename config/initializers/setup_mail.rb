22af9fe0bbaf92dc690d7e2837bb942da0198063
Tania Jacob <tanzjacob@gmail.com>
2016-10-11 12:13:35 +1300

    Set mailer config

--- config/initializers/setup_mail.rb
+++
@@ -1,8 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => ENV["MAIL_ADDRESS"],
-  :port                 => ENV["PORT"],
-  :user_name            => ENV["MAIL_USERNAME"],
-  :password             => ENV["MAIL_PASSWORD"],
-  :authentication       => ENV["AUTHENTICATION"],
-  :enable_starttls_auto => true
-}
