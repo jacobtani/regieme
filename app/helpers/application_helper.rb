2977e59c2407462475860a2256a01c3e77a04049
Tania Jacob <tanzjacob@gmail.com>
2016-10-14 23:40:46 +1300

    Minor tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
- def flash_class(level)
+  def flash_class(level)
     case level.to_sym
-    when :notice then "warning-alert"
-    when :success then "success-alert"
-    when :error then "danger-alert"
-    when :alert then "danger-alert"
+      when :notice then "warning-alert"
+      when :success then "success-alert"
+      when :error then "danger-alert"
+      when :alert then "danger-alert"
     end
   end
 end
