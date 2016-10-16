aa134f647c75ec77a541485cb8c48648ed4515df
Tania Jacob <tanzjacob@gmail.com>
2016-10-17 10:01:23 +1300

    Move JSON files to public/JSON and update controller refs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def accomodation
-    @accomodation = JSON.parse((File.read("#{Rails.root}/app/assets/javascripts/accomodation.json")))
+    @accomodation = JSON.parse((File.read("#{Rails.root}/public/json/accomodation.json")))
   end
 
   def contact_us
@@ -15,24 +15,24 @@ class PagesController < ApplicationController
   end
 
   def faq
-    @faqs = JSON.parse((File.read("#{Rails.root}/app/assets/javascripts/faq.json")))
+    @faqs = JSON.parse((File.read("#{Rails.root}/public/json/faq.json")))
   end
 
   def gift_registry
   end
 
   def our_story
-    @our_story_events = JSON.parse((File.read("#{Rails.root}/app/assets/javascripts/our_story.json")))
+    @our_story_events = JSON.parse((File.read("#{Rails.root}/public/json/our_story.json")))
   end
 
   def people
-    @main_people = JSON.parse((File.read("#{Rails.root}/app/assets/javascripts/main_party.json")))
-    @bridesmaids_groomsmen = JSON.parse((File.read("#{Rails.root}/app/assets/javascripts/bridesmaids_groomsmen.json")))
-    @wedding_party = JSON.parse((File.read("#{Rails.root}/app/assets/javascripts/wedding_party.json")))
+    @main_people = JSON.parse((File.read("#{Rails.root}/public/json/main_party.json")))
+    @bridesmaids_groomsmen = JSON.parse((File.read("#{Rails.root}/public/json/bridesmaids_groomsmen.json")))
+    @wedding_party = JSON.parse((File.read("#{Rails.root}/public/json/wedding_party.json")))
   end
 
   def wedding_festivities
-    @the_details = JSON.parse((File.read("#{Rails.root}/app/assets/javascripts/the_details.json")))
+    @the_details = JSON.parse((File.read("#{Rails.root}/public/json/the_details.json")))
   end
 
   def welcome
