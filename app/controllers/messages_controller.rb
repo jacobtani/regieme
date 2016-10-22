0705e8c763d25de7a19b265fa956fda9e7f637d0
Tania Jacob <tanzjacob@gmail.com>
2016-10-22 14:47:52 +1300

    Fix contact us page

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,12 +7,15 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new message_params
-    if @message.valid?
-      SendEnquiryEmail.perform!(guest: @guest, message: @message)
-      flash[:notice] = "Message sent! Thank you for contacting us."
-      redirect_to contact_us_path
-    else
-      render "new"
+    respond_to do |format|
+      if @message.valid? && @guest.present?
+        SendEnquiryEmail.perform!(guest: @guest, message: @message)
+        flash[:notice] = "Message sent! Thank you for contacting us."
+        format.html { redirect_to contact_us_path }
+      else
+        flash[:error] = "Unfortunately your name could not be found in the guest list."
+        format.html { redirect_to contact_us_path }
+      end
     end
   end
 
@@ -21,10 +24,7 @@ class MessagesController < ApplicationController
   def find_guest
    @guest_full_name = params[:message][:full_name].split
    @guest = Guest.where("first_name = ? AND surname = ?", @guest_full_name[0], @guest_full_name[1]).first
-
-   if @guest.email == "NULL"
-      @guest.update(email: params[:message][:email])
-   end
+   @guest.update(email: params[:message][:email]) if @guest
   end
 
   def message_params
