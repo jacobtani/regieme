d51b75aa93dc74d58593f2255b9c7128899071f4
Tania Jacob <tanzjacob@gmail.com>
2016-10-22 14:37:03 +1300

    Change to use single quotes

--- app/assets/javascripts/faq.js
+++ app/assets/javascripts/faq.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-  $(".question").click(function (e) {
+  $('.question').click(function (e) {
     e.preventDefault();
-    var content_id = $(this).attr("class");
-    $(".answer").addClass("hidden");
+    var content_id = $(this).attr('class');
+    $('.answer').addClass('hidden');
     var numb = content_id.match(/\d/g).join("");
     var total = ".a" + numb;
-    $(total).removeClass("hidden");
+    $(total).removeClass('hidden');
   });
 });
