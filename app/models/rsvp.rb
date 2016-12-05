82b2aa49da64c757f29c4709f5f3d66ac4d8e8b7
Tania Jacob <tanzjacob@gmail.com>
2016-12-05 15:09:47 +1300

    Tidy up css and html to use descriptive lists and bootstrap mixin

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,5 +1,5 @@
 class Rsvp < ActiveRecord::Base
-  validates :attending, :email, presence: true
+  validates :attending, :email, :guest_name, presence: true
   validates_presence_of :main_meal_preference, :if => 'attending == "Yes"', :message => "You need to select a main meal preference from the dropdown"
   validates_presence_of :dessert_preference, :if => 'attending == "Yes"', :message => "You need to select a dessert preference from the dropdown"
   attr_accessor :full_name
