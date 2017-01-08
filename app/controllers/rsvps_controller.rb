1a09a942040891f5cffb7d080e9b2c7e33ee68e6
Tania Jacob <tanzjacob@gmail.com>
2017-01-09 09:58:51 +1300

    Update RSVP to not redirect to signup page

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,5 +1,5 @@
 class RsvpsController < ApplicationController
-  before_action :authenticate_user!, except: [:new, :create]
+  before_action :authenticate_user!, only: [:destroy, :index, :update, :edit]
   before_action :set_rsvp, only: [:edit, :update, :show, :destroy]
 
   def index
